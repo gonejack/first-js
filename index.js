8742f71846f0abb8ac93ae7897630ed6fd543176
gonejack <igonejack@gmail.com>
2020-11-03 16:37:44 +0800

    update

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const [c0, c9, ca] = '09A'.split('').map(c => c.charCodeAt(0))
+async function test() {
+    const p1 = new Promise((res, rej) => res(1))
+    const p2 = new Promise((res, rej) => res(2))
+    const p3 = new Promise((res, rej) => res(3))
 
-// 十六进制转十进制，输入字符串为全大写
-function h2d(hex) {
-    let d = 0;
-    for (let i = 0; i < hex.length; i++) {
-        const x = hex.charCodeAt(i)
-        d = d << 4 | (x > c9 ? x - (ca - 10) : x - c0)
-    }
-    return d;
+    await new Promise(res => setTimeout(res, 2e3))
+
+    console.log(await Promise.race([p1, p2, p3]))
 }
 
-console.log(h2d("1A5C") === 6748)
-console.log(h2d("271") === 625)
+test()
